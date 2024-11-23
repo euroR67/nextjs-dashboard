f014d6243f967cc888bc33e93b20b0e51b8658fb
euroR67 <mchamaev67@gmail.com>
2024-11-23 01:23:26 +0100

    chapter 3 finished

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Roboto_Mono as RobotoMonoFont } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const robotoMono = RobotoMonoFont({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+});
