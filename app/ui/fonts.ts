f6abce62a76689e197a9b75e5aa4ce41ff8bdca0
euroR67 <mchamaev67@gmail.com>
2025-01-06 12:18:46 +0100

    add routes

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,8 +1,13 @@
-import { Inter, Roboto_Mono as RobotoMonoFont } from 'next/font/google';
+import { Inter, Roboto_Mono as RobotoMonoFont, Lusitana } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
 
 export const robotoMono = RobotoMonoFont({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+});
+
+export const lusitana = Lusitana({
     weight: ['400', '700'],
     subsets: ['latin'],
 });
